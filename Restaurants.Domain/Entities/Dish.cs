301ced020614335e55035b30135fffa482cd3bff
Kakha <kakhaber.toklikishvili@gmail.com>
2024-07-24 15:58:07 +0400

    Add project files.

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurants.Domain.Entities;
+
+public class Dish
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    internal RestaurantsDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder.UseSqlServer("RestaurantsDbConnstring"));
+    }
+}
