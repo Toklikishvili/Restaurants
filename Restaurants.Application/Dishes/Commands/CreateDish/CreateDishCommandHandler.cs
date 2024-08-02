9f9209d163335fabc7177ac625742d3cec8303e2
Kakha <kakhaber.toklikishvili@gmail.com>
2024-08-02 17:10:13 +0400

    Location header for subentity

--- Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
+++ Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
@@ -7,7 +7,7 @@ using Restaurants.Domain.Repsitories;
 
 namespace Restaurants.Application.Dishes.Commands.CreateDish;
 
-public class CreateDishCommandHandler : IRequestHandler<CreateDishCommand>
+public class CreateDishCommandHandler : IRequestHandler<CreateDishCommand, int>
 {
     private readonly ILogger<CreateDishCommandHandler> _logger;
     private readonly IRestaurantsRepository _restaurantsRepository;
@@ -22,7 +22,7 @@ public class CreateDishCommandHandler : IRequestHandler<CreateDishCommand>
         _mapper = mapper;
     }
 
-    public async Task Handle(CreateDishCommand request , CancellationToken cancellationToken)
+    public async Task<int> Handle(CreateDishCommand request , CancellationToken cancellationToken)
     {
         _logger.LogInformation("Creating a new restaurant {@Restaurant}" , request);
         var restaurant = await _restaurantsRepository.GetByIdAsync(request.RestaurantId);
@@ -30,6 +30,6 @@ public class CreateDishCommandHandler : IRequestHandler<CreateDishCommand>
 
         var dish = _mapper.Map<Dish>(request);
 
-        await _dishiesRepository.Create(dish);
+       return await _dishiesRepository.Create(dish);
     }
 }
