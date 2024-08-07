a011a84e87bcb1044a26ebe96a566961485eac72
Kakha <kakhaber.toklikishvili@gmail.com>
2024-08-07 14:54:02 +0400

    Role based authorization

--- Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -1,4 +1,6 @@
-﻿using Restaurants.Domain.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using Restaurants.Domain.Constants;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Persistence;
 
 namespace Restaurants.Infrastructure.Seeders;
@@ -11,11 +13,31 @@ internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSee
         {
             if (!dbContext.Restaurants.Any())
             {
+
                 var restaurants = GetRestaurants();
                 dbContext.AddRange(restaurants);
                 await dbContext.SaveChangesAsync();
             }
+
+            if (!dbContext.Roles.Any())
+            {
+                var roles = GetRoles();
+                dbContext.Roles.AddRange(roles);
+                await dbContext.SaveChangesAsync();
+            }
         }
+    }
+
+    private IEnumerable<IdentityRole> GetRoles()
+    {
+        List<IdentityRole> roles =
+            [
+               new (UserRoles.User),
+               new (UserRoles.Owner),
+               new (UserRoles.Admin),
+            ];
+
+        return roles;
     }
 
     private static IEnumerable<Restaurant> GetRestaurants()
