02897a75ae2d85116f461d46bc6fe73108e13382
Kakha <kakhaber.toklikishvili@gmail.com>
2024-08-14 21:52:11 +0400

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private readonly int [] allowPageSize = [5 , 10 , 15 , 30];
+
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowPageSize.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join("," , allowPageSize)}]");
+    }
+}
